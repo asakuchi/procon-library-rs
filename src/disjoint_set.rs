283fa8b64ed71acc3be79388a1d2e16046ada268
asakuchi <paleapricot.asakuchi@gmail.com>
2023-11-14 03:52:43 +0900

    重みつきUnionFind

--- src/disjoint_set.rs
+++ src/disjoint_set.rs
@@ -2,6 +2,8 @@
 pub struct DisjointSet {
     rank: Vec<usize>,
     parent: Vec<usize>,
+    /// 重み
+    diff_weight: Vec<isize>,
 }
 
 impl DisjointSet {
@@ -9,33 +11,64 @@ impl DisjointSet {
         DisjointSet {
             rank: vec![0; n],
             parent: (0..n).collect::<Vec<_>>(),
+            diff_weight: vec![0; n],
         }
     }
 
     pub fn find_set(&mut self, x: usize) -> usize {
         if x != self.parent[x] {
-            self.parent[x] = self.find_set(self.parent[x]);
+            let r = self.find_set(self.parent[x]);
+
+            self.diff_weight[x] += self.diff_weight[self.parent[x]];
+
+            self.parent[x] = r;
         }
 
         self.parent[x]
     }
 
     pub fn unite(&mut self, x: usize, y: usize) {
-        let a = self.find_set(self.parent[x]);
-        let b = self.find_set(self.parent[y]);
+        self.unite_weight(x, y, 0);
+    }
 
-        if self.rank[a] > self.rank[b] {
-            self.parent[b] = a;
-        } else {
-            self.parent[a] = b;
-            if self.rank[a] == self.rank[b] {
-                self.rank[b] += 1;
-            }
+    pub fn unite_weight(&mut self, x: usize, y: usize, w: isize) {
+        let mut w = w + self.weight(x) - self.weight(y);
+
+        let mut x = self.find_set(self.parent[x]);
+        let mut y = self.find_set(self.parent[y]);
+
+        // rank[a] > rank[b] となるように swap
+        if self.rank[x] < self.rank[y] {
+            std::mem::swap(&mut x, &mut y);
+            w = -w;
         }
+
+        if self.rank[x] == self.rank[y] {
+            self.rank[x] += 1;
+        }
+
+        self.parent[y] = x;
+
+        self.diff_weight[y] = w;
     }
 
     pub fn same(&mut self, x: usize, y: usize) -> bool {
         self.find_set(x) == self.find_set(y)
+    }
+
+    pub fn weight(&mut self, x: usize) -> isize {
+        self.find_set(x);
+
+        self.diff_weight[x]
+    }
+
+    ///
+    /// ノード間の距離を返す
+    ///
+    /// （重みつき UnionFind）
+    ///
+    pub fn diff(&mut self, x: usize, y: usize) -> isize {
+        self.weight(y) - self.weight(x)
     }
 }
 
